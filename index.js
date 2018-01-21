cef07a2ee161583412d41af41371913278a80a33
Brian Stumbaugh <brianstumbaugh@brians-mbp.attlocal.net>
2018-01-21 17:24:24 -0600

    starting with passport

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'bubba' });
-});
+passport.use(new GoogleStrategy());
 
 
 // Heroku determines what port we will use
