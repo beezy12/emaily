93db1caf13f96bc4a9e9ca0afa1e689596e88120
Brian Stumbaugh <brianstumbaugh@brians-mbp.attlocal.net>
2018-02-11 23:38:45 -0600

    last issue about the 'semver' was resolved by googling about it and basically deleting node and reinstalling through homebrew, per a suggestion from someone online. that worked, but when trying to view the page on heroku, got some 'Error: redirect_uri_mismatch' error, which is an http vs https error, and is resolved by adding 'proxy: true' to my googleStrategy in passport

--- services/passport.js
+++ services/passport.js
@@ -31,7 +31,8 @@ passport.use(
     new GoogleStrategy({
             clientID: keys.googleClientID,
             clientSecret: keys.googleClientSecret,
-            callbackURL: '/auth/google/callback'
+            callbackURL: '/auth/google/callback',
+            proxy: true
         },
         (accessToken, refreshToken, profile, done) => {
             User.findOne({ googleId: profile.id }) // checks to see if user record already exists first. this is async
