ab700b8afe2ad90648aa2cf8c31fbafe878768c4
Brian Stumbaugh <brian.stumbuagh@gmail.com>
2018-05-30 00:06:11 -0500

    billing is pretty much set up. end of video 107 and the stripe section

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -2,6 +2,8 @@ import { FETCH_USER } from '../actions/types';
 
 // returning || false because it was returning a set of empty strings when the user was logged out. empty strings
 // are equal to false...so I am saying if it's an empty string, set it to explicitly to false
+
+// anytime an action comes across with a type of FETCH_USER, return the action's payload (the updated user model)
 export default function(state = null, action) {
   switch (action.type) {
     case FETCH_USER:
