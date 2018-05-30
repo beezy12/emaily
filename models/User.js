770c85185bd2d014d0c7df8c13eeb3a67013e6db
Brian Stumbaugh <brian.stumbuagh@gmail.com>
2018-05-29 23:39:32 -0500

    added check to make sure user is logged in, before I start adding credits

--- models/User.js
+++ models/User.js
@@ -1,14 +1,18 @@
 const mongoose = require('mongoose');
 //const Schema = mongoose.Schema;
-// es6 destructuring below, it's the same thing as above. It's saying: the mongoose object has a property called Schema, pull it out and assign it to a variable called Schema
+// es6 destructuring below, it's the same thing as above.
+// It's saying: the mongoose object has a property called Schema, pull it out and assign it to a variable called Schema
 const { Schema } = mongoose;
 
 // schema
 const userSchema = new Schema ({
-  googleId: String
+  googleId: String,
+  credits: { type: Number, default: 0}
 });
 
-// making a model class, telling mongoose that this new collection has been created....THIS LOADS THE SCHEMA INTO MONGOOSE, so therefore I won't have to require it everywhere...which would caues problems because mongoose would think you are creating a new 'users' collection everywhere that it's required.
+// making a model class, telling mongoose that this new collection has been created....
+//THIS LOADS THE SCHEMA INTO MONGOOSE, so therefore I won't have to require it everywhere...
+// which would caues problems because mongoose would think you are creating a new 'users' collection everywhere that it's required.
 // we are creating a collection and here calling it 'users'.
 // where the first argument is the name of the collection, and the second argument is the name of the schema.
 // **** IMPORTANT: mongoose will only make the collection if it does not already exist, and it won't overwrite it
