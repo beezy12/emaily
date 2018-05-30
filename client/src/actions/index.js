3099d166e4d126ae6b35658cc32b66f296006535
Brian Stumbaugh <brian.stumbuagh@gmail.com>
2018-05-29 20:52:04 -0500

    created action creator to handle the token Stripe gives me after a purchase is made. route is not set up yet

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -6,6 +6,15 @@ import { FETCH_USER } from './types';
 // button or user profile...do this using componentDidMount in App.js
 export const fetchUser = () => async dispatch => {
   const res = await axios.get('/api/currentUser');
+  // this is the action to dispatch AFTER I get a response from the server. the response is the User model.
+  // look for FETCH_USER in the auth_reducer file
+  dispatch({ type: FETCH_USER, payload: res.data });
+};
 
+// action creator handleToken here
+export const handleToken = (token) => async dispatch => {
+  const res = await axios.post('/api/stripe', token);
+  // I can use the same action FETCH_USER after I send a post, because I want to see how many tokens the
+  // user has now, so again the response is the User model
   dispatch({ type: FETCH_USER, payload: res.data });
 };
