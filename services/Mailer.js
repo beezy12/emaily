afa9c82eb31477a65cafc7ad321c04122115fce4
Brian Stumbaugh <brianstumbaugh@brians-mbp.attlocal.net>
2018-06-26 12:33:31 -0500

    added last functionality to send email info to Sendgrid

--- services/Mailer.js
+++ services/Mailer.js
@@ -6,6 +6,7 @@ class Mailer extends helper.Mail {
   constructor({ subject, recipients }, content) {
     super();
 
+    this.sgApi = sendgrid(keys.sendGridKey)
     this.from_email = new helper.Email('no-reply@email.com');
     this.subject = subject;
     this.body = new helper.Content('text/html', content);
@@ -42,6 +43,18 @@ class Mailer extends helper.Mail {
       personalize.addTo(recipient);
     });
     this.addPersonalization(personalize);
+  }
+
+  // sends email to the Sendgrid api
+  async send() {
+    const request = this.sgApi.emptyRequest({
+      method: 'POST',
+      path: '/v3/mail/send',
+      body: this.toJSON()
+    });
+
+    const response = this.sgApi.API(request);
+    return response;
   }
 
 }
