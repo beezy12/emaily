a4f05520b54a86af93ac432f35203f89e4855f96
Brian Stumbaugh <brian.stumbaugh@gmail.com>
2018-07-05 20:32:05 -0500

    setup basic field components

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
-import { reduxForm } from 'redux-form';
+import { reduxForm, Field } from 'redux-form';
 
+// handleSubmit is a built-in function of redux-form
+// name property on Field is the key that will be store in the redux object
 class SurveyForm extends Component {
   render() {
     return(
       <div>
-        surveyyyyyy formmmm
+        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+          <Field
+            type="text"
+            name="surveyTitle"
+            component="input"
+          />
+          <button type="submit">Submit</button>
+        </form>
       </div>
     );
   }
