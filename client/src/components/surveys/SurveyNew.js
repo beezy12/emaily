d687f68a6082feffc9552851119edd10d484347f
Brian Stumbaugh <brian.stumbaugh@gmail.com>
2018-07-05 19:03:39 -0500

    start of set up for surveys form. installed redux-form and built basic new survey component

---
+++ client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+// SurveyNew is the parent component for both SurveyForm and SurveyFormReview
+
+class SurveyNew extends Component {
+  render() {
+    return (
+      <div>
+        SURVEY NEW!
+      </div>
+    );
+  }
+}
+
+export default SurveyNew;
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux';
+// reducer is just the name of the function from redux-form. es6 lets you rename it in this way
+import { reducer as reduxForm } from 'redux-form';
 import authReducer from './authReducer';
 
 export default combineReducers({
-  auth: authReducer
+  auth: authReducer,
+  form: reduxForm
 });
