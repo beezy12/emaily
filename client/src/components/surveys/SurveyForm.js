0e8a614f5e09effae6b1e34b7b0f2f5f77b09648
Brian Stumbaugh <brian.stumbaugh@gmail.com>
2018-07-05 21:24:48 -0500

    refactored fields to be more flexible

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import SurveyField from './SurveyField';
+import _ from 'lodash';
+
+
+const FIELDS = [
+  { label: "Survey Title", name: "title" },
+  { label: "Subject Line", name: "subject" },
+  { label: "Email Body", name: "body" },
+  { label: "Recipient List", name: "emails"}
+];
 
 // handleSubmit is a built-in function of redux-form
 // name property on Field is the key that will be store in the redux object
@@ -8,14 +17,9 @@ import SurveyField from './SurveyField';
 // example, I added label. this will make each field unique and customizable
 class SurveyForm extends Component {
   renderFields() {
-    return(
-      <div>
-        <Field label="Survey Title" type="text" name="title" component={SurveyField} />
-        <Field label="Subject Line" type="text" name="subject" component={SurveyField} />
-        <Field label="Email Body" type="text" name="body" component={SurveyField} />
-        <Field label="Recipient List" type="text" name="emails" component={SurveyField} />
-      </div>
-    )
+    return _.map(FIELDS, field => {
+      return <Field key={field.name} component={SurveyField} type="text" label={field.label} name={field.name} />
+    });
   }
 
   render() {
