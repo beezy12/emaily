804871ef536491e8353c9327bdb54c029b314d46
Brian Stumbaugh <brian.stumbaugh@gmail.com>
2018-07-05 20:47:28 -0500

    wiring up my custom survey fields

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
+import SurveyField from './SurveyField';
 
 // handleSubmit is a built-in function of redux-form
 // name property on Field is the key that will be store in the redux object
 class SurveyForm extends Component {
+  renderFields() {
+    return(
+      <div>
+        <Field type="text" name="surveyTitle" component={SurveyField} />
+      </div>
+    )
+  }
+
   render() {
     return(
       <div>
         <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
-          <Field
-            type="text"
-            name="surveyTitle"
-            component="input"
-          />
+          {this.renderFields()}
           <button type="submit">Submit</button>
         </form>
       </div>
