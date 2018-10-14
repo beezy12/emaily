a075da60fe4b3ef287525b5b0ed58ae9071079dc
Brian Stumbaugh <brian.stumbaugh@gmail.com>
2018-10-14 10:43:05 -0500

    added button to submit form values and set up action creator to change state, but it's not doing anything yet. next steps are to send these values to the server and redirect user upon doing so. also as of now if a user hits cancel and then cancel again to go back to the main screen, and then clicks the + button to make a survey....our values are STILL persisting, so those need to be cleared out as well.

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -8,6 +8,7 @@ export const fetchUser = () => async dispatch => {
   const res = await axios.get('/api/currentUser');
   // this is the action to dispatch AFTER I get a response from the server. the response is the User model.
   // look for FETCH_USER in the auth_reducer file
+  // redux will always expect an object to be returned with a 'type', or it will crash
   dispatch({ type: FETCH_USER, payload: res.data });
 };
 
@@ -17,4 +18,9 @@ export const handleToken = (token) => async dispatch => {
   // I can use the same action FETCH_USER after I send a post, because I want to see how many tokens the
   // user has now, so again the response is the User model
   dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+
+export const submitSurvey = values => {
+  return { type: 'submit_survey' };
 };
--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -2,9 +2,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import formFields from './formFields';
+import * as actions from '../../actions';
 
 // destructuring and pulling just the onCancel function off of props
-const SurveyFormReview = ({ onCancel, formValues }) => {
+// submitSurvey is an action creator that I imported in as a prop
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
 
   // you could use es6 destructuring here and just pull name and label off of the 'field' object
   const reviewFields = formFields.map(field => {
@@ -20,8 +22,18 @@ const SurveyFormReview = ({ onCancel, formValues }) => {
     <div>
       <h5>Please confirm your entries</h5>
       {reviewFields}
-      <button className="yellow darken-3 btn-flat" onClick={onCancel} >
+      <button className="yellow darken-3 white-text btn-flat" onClick={onCancel} >
         Cancel
+      </button>
+
+      {/* this 'submitSurvey' is my redux action in the actions/index.js file. it's imported with actions*/}
+      {/*had to wrap it in an arrow function, otherwise it would be executed right away*/}
+      <button
+        onClick={() => submitSurvey(formValues)}
+        className="green btn-flat right white-text"
+      >
+        Send Survey
+        <i className="material-icons right">email</i>
       </button>
     </div>
   );
@@ -39,4 +51,4 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(SurveyFormReview);
+export default connect(mapStateToProps, actions)(SurveyFormReview);
